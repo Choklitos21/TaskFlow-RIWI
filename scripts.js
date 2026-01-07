efe501f09b211ac6f6b3f1d5c35c8a92db6b767d
Choklitos21 <diego.mm09@hotmail.com>
2026-01-07 14:08:03 -0500

    set variables to work with DOM

--- scripts.js
+++ scripts.js
@@ -1,3 +1,21 @@
+let task = {
+    title: String,
+    description: String,
+    priority: ["high", "medium", "low"],
+    status: ["completed", "in process", "pending"]
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('¡El DOM está listo!'); // Muestra en la consola del navegador
+
+    // Para mostrar en la página (ej. dentro de un <p id="mensaje"></p>)
+    const elementoMensaje = document.getElementById('mensaje');
+    if (elementoMensaje) {
+        elementoMensaje.textContent = '¡Hola desde JavaScript al cargar la página!';
+    }
+});
+
 function createTask() {
-    return false;
+    newTask = new task()
+
 }
