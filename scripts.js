0c940656643e11f52007ccba7bd6a73197567d29
Choklitos21 <diego.mm09@hotmail.com>
2026-01-07 15:41:31 -0500

    create function to add tasks

--- scripts.js
+++ scripts.js
@@ -1,21 +1,46 @@
-let task = {
-    title: String,
-    description: String,
-    priority: ["high", "medium", "low"],
-    status: ["completed", "in process", "pending"]
-}
 
-document.addEventListener('DOMContentLoaded', function() {
-    console.log('¡El DOM está listo!'); // Muestra en la consola del navegador
 
-    // Para mostrar en la página (ej. dentro de un <p id="mensaje"></p>)
-    const elementoMensaje = document.getElementById('mensaje');
-    if (elementoMensaje) {
-        elementoMensaje.textContent = '¡Hola desde JavaScript al cargar la página!';
+document.getElementById('form').addEventListener('submit', function(event) {
+    // 1. Prevenir el envío por defecto
+    event.preventDefault();
+
+    // 2. Crear un objeto FormData para obtener todos los datos
+    const formData = new FormData(this); // 'this' se refiere al formulario
+
+    // 3. Obtener valores específicos
+    const title = formData.get('title');
+    const description = formData.get('description');
+    const priority = formData.get('priority');
+    const status = 'in process'
+
+    if (!title) {
+        alert("Insert a title name")
+        return false
+    } else if (!description) {
+        alert("Insert a description")
+        return false
+    } else if (priority === "#") {
+        alert("Choose a priority")
+        return false
     }
+
+    const taskList = document.getElementById('cards-container')
+
+    const newTask = document.createElement('div')
+    newTask.innerHTML = '<div class="card">\n' +
+        '                <strong class="title">' + title + '</strong>\n' +
+        '                <span class="priority">' + priority + '</span>\n' +
+        '                <select name="status" id="status">' +
+        '                 <option value="#" selected>' + status + '</option>' +
+        '                 <option value="#" selected>High</option>'+
+        '                 <option value="#" selected>Medium</option>'+
+        '                 <option value="#" selected>Low</option>'+
+        '                 </select>' +
+        '                <p>' + description + '</p>\n' +
+        '                <button id="delete">Delete</button>\n' +
+        '            </div>'
+
+    taskList.appendChild(newTask)
+    this.reset();
+
 });
-
-function createTask() {
-    newTask = new task()
-
-}
